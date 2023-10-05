a14e0052a510bf0b5b7847ebfd4024943142ae20
younsoo <tweatershin@gmail.com>
2023-10-05 10:30:47 +0900

    board detail

--- mydjango_board/board/urls.py
+++ mydjango_board/board/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('<int:board_id>', views.board_detail, name="detail"),
+
     #     path('', views.board_list, name='index'),
     #     path('<int:board_id>/',
     #          views.board_detail,
--- mydjango_board/board/views.py
+++ mydjango_board/board/views.py
@@ -6,7 +6,16 @@ from .models import Board, Comment
 
 def index(request):
     board_list = Board.objects.all()
-    return render(request, "board/index.html", {'board_list': board_list})
+    return render(request, "board/index.html",
+                  {'board_list': board_list})
+
+
+def board_detail(request, board_id):
+    board = Board.objects.get(pk=board_id)
+
+    return render(request,
+                  "board/detail.html",
+                  {'board': board})
 
 # def index(request):
 #     # return HttpResponse("Hello World, I am Younsoo!")
@@ -16,65 +25,65 @@ def index(request):
 #                   {'name': '유저', 'sample': sample_list})
 
 
-"""
-board_list라는 view 함수를 만들고 /board 로 접속하면 
-게시글에 대한 전체 게시글을 리스트 (HTML: ul, li 태그 이용)로 보여주세요. 
-"""
+# """
+# board_list라는 view 함수를 만들고 /board 로 접속하면
+# 게시글에 대한 전체 게시글을 리스트 (HTML: ul, li 태그 이용)로 보여주세요.
+# """
 
 
-def board_list(request):
-    qs = Board.objects.all()
+# def board_list(request):
+#     qs = Board.objects.all()
 
-    html = ""
-    for board in qs:
-        html += f"<li><a href='/board/{board.id}'>\
-            {board.title}\
-                </a></li>"
-    html = f"<ul>{html}</ul>"
+#     html = ""
+#     for board in qs:
+#         html += f"<li><a href='/board/{board.id}'>\
+#             {board.title}\
+#                 </a></li>"
+#     html = f"<ul>{html}</ul>"
 
-    return HttpResponse(html)
-
-
-"""/board/comments로 접속하면 모든 댓글을 조회하도록 해주세요. 
-    조회내용에 대한 형식은 다음과 같습니다.
-    (<ul> 
-    <li>코멘트id | 댓글내용 | board_id </li>
-    …
-    </ul>)
-"""
+#     return HttpResponse(html)
 
 
-def comment_list(request):
-    qs = Comment.objects.all()
-
-    html = ""
-    for comment in qs:
-        html += f"<li>{comment.id} | \
-            {comment.content} | {comment.board_id} </li>"
-    html = f"<ul>{html}</ul>"
-
-    return HttpResponse(html)
+# """/board/comments로 접속하면 모든 댓글을 조회하도록 해주세요.
+#     조회내용에 대한 형식은 다음과 같습니다.
+#     (<ul>
+#     <li>코멘트id | 댓글내용 | board_id </li>
+#     …
+#     </ul>)
+# """
 
 
-"""
-각 게시글을 클릭 가능하게 해주시고, 
-클릭에 대한 url은 /board/<id> 로 해주세요. 
-(<id>는 board에 대한 id(pk)값
-"""
+# def comment_list(request):
+#     qs = Comment.objects.all()
 
-# (*args, **kwargs)
+#     html = ""
+#     for comment in qs:
+#         html += f"<li>{comment.id} | \
+#             {comment.content} | {comment.board_id} </li>"
+#     html = f"<ul>{html}</ul>"
+
+#     return HttpResponse(html)
 
 
-def board_detail(request, board_id):
-    qs = Board.objects.get(id=board_id)
-    comment_list = qs.comment_set.all()
+# """
+# 각 게시글을 클릭 가능하게 해주시고,
+# 클릭에 대한 url은 /board/<id> 로 해주세요.
+# (<id>는 board에 대한 id(pk)값
+# """
 
-    html = f"<h1>{qs.title}</h1> \
-        <div>{qs.content}</div>"
+# # (*args, **kwargs)
 
-    html += "<ul>"
-    for comment in comment_list:
-        html += f"<li>{comment.content}</li>"
-    html += "</ul>"
 
-    return HttpResponse(html)
+# def board_detail(request, board_id):
+#     qs = Board.objects.get(id=board_id)
+#     comment_list = qs.comment_set.all()
+
+#     html = f"<h1>{qs.title}</h1> \
+#         <div>{qs.content}</div>"
+
+#     html += "<ul>"
+#     for comment in comment_list:
+#         html += f"<li>{comment.content}</li>"
+#     html += "</ul>"
+
+#     return HttpResponse(html)
